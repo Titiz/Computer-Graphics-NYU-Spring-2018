43cfe4005864cc25fe70eb058410421a9cd64eeb
titiz <gtitiz@gmail.com>
2018-01-31 22:02:06 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function fitToContainer(canvas){
+  canvas.style.width='100%';
+  canvas.style.height='100%';
+  canvas.width  = canvas.offsetWidth;
+  canvas.height = canvas.offsetHeight;
+}
+
+
+
+var c = document.getElementById("chw0");
+var ctx = c.getContext("2d");
+fitToContainer(c);
